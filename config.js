207d6ad9f25ef7cc0924024562a321fec5ca8bf5
gooselycharm <40129530+gooselycharm@users.noreply.github.com>
2018-06-09 16:19:08 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports={
+  consumer_key: 'xG9UCBtnvDIrWW1zS5FETaI8V',
+  consumer_secret: 'xLhCq7g2MhsrndO04S14zfPSAQPtm64gwEWFaAuKMFxdACZyvl',
+  access_token: '1005577784784064512-JSbSqfOmtG6KKl73NjKgJrvkXY5Qy3',
+  access_token_secret: 'ZAcPAgUm1v85P4xWUpO7YbbaUmtTCb5am2vJw6TsiF0vg',
+
+}
---
+++ server.js
@@ -0,0 +1,60 @@
+var Twit = require('twit')
+
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twit(config);
+
+function random_from_array(images){
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function upload_random_image(images){
+  console.log('Opening an image...');
+  var image_path = path.join(__dirname, '/pics/' + random_from_array(images)),
+      b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+  console.log('Uploading an image...');
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:');
+      console.log(err);
+    }
+    else{
+      console.log('Image uploaded!');
+      console.log('Now tweeting it...');
+
+      T.post('statuses/update', {
+        media_ids: new Array(data.media_id_string)
+      },
+        function(err, data, response) {
+          if (err){
+            console.log('ERROR:');
+            console.log(err);
+          }
+          else{
+            console.log('Posted an image!');
+          }
+        }
+      );
+    }
+  });
+}
+fs.readdir(__dirname + '/pics/', function(err, files) {
+  if (err){
+    console.log(err);
+  }
+  else{
+    var images = [];
+    files.forEach(function(f) {
+      images.push(f);
+    });
+
+    setInterval(function(){
+      upload_random_image(images);
+    }, 60000*60*24);
+  }
+});
